7613ba55940c25dd8bd0021dbf3ba9b592a775f6
AhmedOumezzine <ahmedoumezzine@outlook.fr>
2017-10-11 23:13:54 +0200

    Validator : String

--- DotNetTN.Tests/Documents/PDF.cs
+++ DotNetTN.Tests/Documents/PDF.cs
@@ -14,10 +14,14 @@ namespace DotNetTN.Tests.Documents
         [TestMethod]
         public void docpdf()
         {
-            PDF doc = new PDF();
+            ConvertPDF doc = new ConvertPDF();
 
-            var data = doc.createPDF("hello");
+            var data = doc.GetBytes("<table style='width: 100 % '> <tr> <th>Firstname</th> <th>Lastname</th> <th>Age</th> </tr> <tr> <td>Jill</td> <td>Smith</td> <td>50</td> </tr> <tr> <td>Eve</td> <td>Jackson</td> <td>94</td> </tr> </table>");
             Console.Write(data);
+            var data2 = doc.GetString(data);
+            Console.Write(data2);
+
+            doc.SavePDF("C:\\Users\\ahmed\\Desktop\\doc\\test2.pdf", data);
         }
     }
 }
---
+++ DotNetTN.Validator/Object/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTN.Validator.Object
+{
+    public static class Extensions
+    {
+        public static IDictionary<string, object> AsDictionary(this object source, BindingFlags bindingAttr = BindingFlags.FlattenHierarchy | BindingFlags.Instance | BindingFlags.Public)
+        {
+            return source.GetType().GetProperties(bindingAttr).ToDictionary
+            (
+                propInfo => propInfo.Name,
+                propInfo => propInfo.GetValue(source, null)
+            );
+        }
+    }
+}
