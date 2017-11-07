6141a3decf0516ed4a418db2633fa1697478c0ec
AhmedOumezzine <ahmedoumezzine@outlook.fr>
2017-11-07 23:14:51 +0100

    check with outlook/gmail/yahoo

--- DotNetTN.Tests/Emails/EmailImapTest.cs
+++ DotNetTN.Tests/Emails/EmailImapTest.cs
@@ -1,11 +1,18 @@
 ﻿using DotNetTN.API.EMAIL;
+using DotNetTN.API.EMAIL.Imap;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace DotNetTN.Tests.Emails
 {
     [TestClass]
     public class EmailImapTest
     {
+        /// <summary>
+        ///  imap-mail.outlook.com
+        ///  imap.gmail.com
+        ///  imap.mail.yahoo.com
+        /// </summary>
         [TestMethod]
         public void GetMessageCount()
         {
@@ -22,6 +29,17 @@ namespace DotNetTN.Tests.Emails
             {
                 int numMessages = 10;
                 var msgs = imap.GetMessages(0, numMessages - 1, false);
+            }
+        }
+
+        [TestMethod]
+        public void Message_With_Attachments()
+        {
+            using (ImapClient imap = new ImapClient("imap-mail.outlook.com", "username@outlook.fr", "password", AuthMethods.Login, 993, true))
+            {
+                var msg = imap.SearchMessages(SearchCondition.Larger(100 * 1000)).FirstOrDefault().Value;
+
+                var att = msg.Attachments;
             }
         }
     }
